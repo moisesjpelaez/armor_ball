014133253cf69e6b021132a2c55dc0beda66deda
Moisés Peláez <moises.jpelaez@gmail.com>
2026-03-27 23:35:35 -0400

    Add Kha logo

--- Sources/arm/scenes/Splash.hx
+++ Sources/arm/scenes/Splash.hx
@@ -2,12 +2,20 @@ package arm.scenes;
 
 import armory.trait.internal.KouiCanvas;
 import koui.elements.Element;
+#if arm_target_n64
+import koui.elements.layouts.AnchorPane;
+import koui.elements.layouts.ColLayout;
+#end
 
 class Splash extends GameScene {
 	@prop var nextScene: String = "MainMenu";
 
 	var canvas: KouiCanvas;
 	var elements: Array<Element>;
+	#if arm_target_n64
+	var nonConsoleContainer: ColLayout;
+	var consoleContainer: AnchorPane;
+	#end
 
 	var currentLogoIndex: Int = 0;
 
@@ -18,6 +26,12 @@ class Splash extends GameScene {
 			canvas = object.getTrait(KouiCanvas);
 			canvas.notifyOnReady(function() {
 				elements = @:privateAccess canvas.getRoot().elements;
+				#if arm_target_n64
+				nonConsoleContainer = canvas.getElementAs(ColLayout, "frameworks/non_console");
+				consoleContainer = canvas.getElementAs(AnchorPane, "frameworks/console");
+				nonConsoleContainer.visible = false;
+				consoleContainer.visible = true;
+				#end
 				init();
 			});
 		});
