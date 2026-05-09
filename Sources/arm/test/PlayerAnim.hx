882800dc1ad12abddb29489bc8d8accb362e01fd
Moisés Peláez <moises.jpelaez@gmail.com>
2026-05-09 01:24:56 -0400

    Cleanup animation script

--- Sources/arm/test/PlayerAnim.hx
+++ Sources/arm/test/PlayerAnim.hx
@@ -21,15 +21,9 @@ class PlayerAnim extends iron.Trait {
 		});
 
 		notifyOnUpdate(function() {
-			#if arm_target_n64
 			if (gamepad.started("a")) anim.play("Run");
 			else if (gamepad.started("b")) anim.play("Jump");
 			else if (gamepad.started("x")) anim.play("Idle");
-			#else
-			if (gamepad.started("a")) anim.play("Run_player_world_map.blend");
-			else if (gamepad.started("b")) anim.play("Jump_player_world_map.blend");
-			else if (gamepad.started("x")) anim.play("Idle_player_world_map.blend");
-			#end
 			else if (gamepad.started("r2")) anim.pause();
 			else if (gamepad.started("l2")) anim.resume();
 		});
